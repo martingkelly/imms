a913eb37c8c62b2866d6b7077b8336d94753dad8
mag <mag@eaf4bba8-40ff-11de-9805-9778bc541ee5>
2003-12-06 22:34:21 +0000

    Cleaned up plugin code.

--- trunk/imms/plugin.cc
+++ trunk/imms/plugin.cc
@@ -22,15 +22,14 @@ using std::cerr;
 // Local vars
 static Imms *imms = NULL;
 unsigned int time_left = 1000, sloppy_skips = 0;
-int last_plpos = -2, cur_plpos, last_pl_length = -1;
-int good_length = 0, song_length = 0, delay = POLL_DELAY;
+int last_plpos = -2, cur_plpos, pl_length = -1;
+int good_length = 0, song_length = 0, delay = 0;
 string cur_path = "", last_path = "";
-bool need_more = false;
+bool need_more = true, spectrum_ok = false;
 
 // Extern from interface.c
 extern VisPlugin imms_vp;
 int &session = imms_vp.xmms_session;
-extern int spectrum_ok;
 
 static enum
 {
@@ -39,15 +38,10 @@ static enum
     FIND_NEXT   = 2
 } state;
 
-inline int imms_get_playlist_length()
-{
-    return xmms_remote_get_playlist_length(session);
-}
-
 // Wrapper that frees memory
 string imms_get_playlist_item(int at)
 {
-    if (at > last_pl_length - 1)
+    if (at > pl_length - 1)
         return "";
     char *tmp = 0;
     while (!tmp)
@@ -81,122 +75,157 @@ void imms_cleanup(void)
 
 void imms_spectrum(uint16_t spectrum[256])
 {
-    if (imms)
+    if (imms && spectrum_ok)
         imms->integrate_spectrum(spectrum);
+}
+
+void do_more_checks()
+{
+    delay = 0;
+
+    // make sure shuffle is disabled
+    if (xmms_remote_is_shuffle(session))
+        xmms_remote_toggle_shuffle(session);
+
+    // update playlist length
+    int new_pl_length = xmms_remote_get_playlist_length(session);
+    if (new_pl_length != pl_length)
+    {
+        pl_length = new_pl_length;
+        imms->playlist_changed(pl_length);
+    }
+
+    // check if xmms is reporting the song length correctly
+    song_length = xmms_remote_get_playlist_time(session, cur_plpos);
+    if (song_length > 1000)
+        good_length++;
+
+    // have imms do it's internal processing
+    imms->pump();
+}
+
+void do_spectrum_checks(int cur_time)
+{
+    const static float skip = 0.15;
+
+    bool _spectrum_ok = (cur_time > song_length * skip
+            && cur_time < song_length * (1 - skip));
+
+    if (spectrum_ok && !_spectrum_ok)
+        imms->stop_spectrum_analysis();
+
+    if (!spectrum_ok && _spectrum_ok)
+        imms->start_spectrum_analysis();
+
+    spectrum_ok = _spectrum_ok;
+}
+
+void do_checks()
+{
+    // if not playing make sure we stopped collecting spectrum statistics
+    if (!xmms_remote_is_playing(session))
+    {
+        if (spectrum_ok)
+            imms->stop_spectrum_analysis();
+        spectrum_ok = false;
+        return;
+    }
+
+    // run these checks less frequently so as not to waste cpu time
+    if (++delay > POLL_DELAY || pl_length < 0 || good_length < 3)
+        do_more_checks();
+
+    bool ending = good_length > 2 && time_left == 0;
+    cur_plpos = xmms_remote_get_playlist_pos(session);
+
+    if (ending || cur_plpos != last_plpos)
+    {
+        cur_path = imms_get_playlist_item(cur_plpos);
+
+        if (ending || last_path != cur_path)
+        {
+            xmms_remote_stop(session);
+            if (last_path == cur_path)
+                xmms_remote_playlist_next(session);
+
+            state = FIND_NEXT;
+            return;
+        }
+
+        last_plpos = cur_plpos;
+    }
+
+    // check the time to catch the end of the song
+    int cur_time = xmms_remote_get_output_time(session);
+    time_left = (song_length - cur_time) / 500;
+
+    do_spectrum_checks(cur_time);
+
+    // if we don't have enough, feed imms more candidates for the next song
+    if (need_more && delay % 2)
+    {
+        int pos = imms_random(xmms_remote_get_playlist_length(session));
+        need_more = imms->add_candidate(pos, imms_get_playlist_item(pos));
+    }
+}
+
+void do_find_next()
+{
+    if (time_left < 8 * (sloppy_skips + 1) * 2)
+        time_left = 0;
+
+    if (last_plpos == -2)
+        last_plpos = cur_plpos - 1;
+
+    cur_plpos = xmms_remote_get_playlist_pos(session);
+    bool forced = ((last_plpos + 1) % pl_length ) != cur_plpos;
+    bool bad = good_length < 3 || song_length <= 30*1000;
+
+    // notify imms that the previous song has ended
+    if (last_path != "")
+        imms->end_song(!time_left, forced, bad);
+
+    // if the song was not directly picked by the user...
+    if (!forced && pl_length > 2)
+    {
+        if (need_more)
+            do { cur_plpos = imms_random(pl_length); }
+            while (imms->add_candidate(cur_plpos,
+                        imms_get_playlist_item(cur_plpos), true));
+
+        // have imms select the next song for us
+        cur_plpos = imms->select_next();
+        cur_path = imms_get_playlist_item(cur_plpos);
+        xmms_remote_set_playlist_pos(session, cur_plpos);
+    }
+
+    // notify imms of the next song
+    imms->start_song(cur_path);
+
+    last_path = cur_path;
+    good_length = 0;
+    need_more = true;
+
+    xmms_remote_play(session);
 }
 
 void imms_poll()
 {
-    if (--delay < 0)
-        delay = POLL_DELAY;
-
     switch (state)
     {
         case BUSY:
             return;
 
         case IDLE:
-            if (last_plpos == -2)
-                last_plpos = xmms_remote_get_playlist_pos(session) - 1;
-
-            if (!xmms_remote_is_playing(session))
-                return;
-
             state = BUSY;
-
-            if (last_pl_length < 0 || !delay)
-            {
-                if (xmms_remote_is_shuffle(session))
-                    xmms_remote_toggle_shuffle(session);
-
-                int pl_length = imms_get_playlist_length();
-                if (pl_length != last_pl_length)
-                {
-                    last_pl_length = pl_length;
-                    imms->playlist_changed(pl_length);
-                }
-            }
-
-            cur_plpos = xmms_remote_get_playlist_pos(session);
-            cur_path = imms_get_playlist_item(cur_plpos);
-
-            if (last_path != cur_path || (good_length > 2 && time_left == 0))
-            {
-                xmms_remote_stop(session);
-                if (last_path == cur_path)
-                    xmms_remote_playlist_next(session);
-
-                state = FIND_NEXT;
-                return;
-            }
-
-            if (good_length < 3 || !delay)
-            {
-                song_length = xmms_remote_get_playlist_time(session,
-                        cur_plpos);
-                if (song_length > 1000)
-                    good_length++;
-            }
-
-            {
-                int cur_time = xmms_remote_get_output_time(session);
-                time_left = (song_length - cur_time) / 500;
-
-                last_plpos = cur_plpos;
-
-                spectrum_ok = (cur_time > song_length * 0.1
-                        && cur_time < song_length * 0.9);
-            }
-
-            if (need_more && delay % 2)
-            {
-                int pos = imms_random(imms_get_playlist_length());
-                need_more = imms->add_candidate(pos,
-                        imms_get_playlist_item(pos));
-            }
-
-            if (!delay)
-                imms->pump();
-
-            state = IDLE;
+            do_checks();
+            if (state == BUSY)
+                state = IDLE;
             return;
 
         case FIND_NEXT:
             state = BUSY;
-
-            if (time_left < 8 * (sloppy_skips + 1) * 2)
-                time_left = 0;
-
-            int pl_length = imms_get_playlist_length();
-            cur_plpos = xmms_remote_get_playlist_pos(session);
-            bool forced = (last_plpos + 1 != cur_plpos) &&
-                (cur_plpos != 0 || last_plpos != pl_length - 1);
-
-            bool bad = good_length < 3 || song_length <= 30*1000;
-
-            if (last_path != "")
-                imms->end_song(!time_left, forced, bad);
-
-            if (!forced && pl_length > 2)
-            {
-                do { cur_plpos = imms_random(pl_length); }
-                while (imms->add_candidate(cur_plpos,
-                            imms_get_playlist_item(cur_plpos), true));
-
-                cur_plpos = imms->select_next();
-                cur_path = imms_get_playlist_item(cur_plpos);
-
-                xmms_remote_set_playlist_pos(session, cur_plpos);
-            }
-
-            imms->start_song(cur_path);
-
-            last_path = cur_path;
-            good_length = 0;
-            need_more = true;
-
-            xmms_remote_play(session);
-
+            do_find_next();
             state = IDLE;
             return;
     }
