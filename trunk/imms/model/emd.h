38ed5940c78daff9f41fd6ad2bae7441a34ccb80
mag <mag@eaf4bba8-40ff-11de-9805-9778bc541ee5>
2009-02-15 04:01:52 +0000

    Add license information to all files.

--- trunk/imms/model/emd.h
+++ trunk/imms/model/emd.h
@@ -1,18 +1,32 @@
 #ifndef _EMD_H
 #define _EMD_H
 /*
-    emd.h
+   emd.h
 
-    Last update: 3/24/98
+   Last update: 3/24/98
 
-    An implementation of the Earth Movers Distance.
-    Based of the solution for the Transportation problem as described in
-    "Introduction to Mathematical Programming" by F. S. Hillier and 
-    G. J. Lieberman, McGraw-Hill, 1990.
+   An implementation of the Earth Movers Distance.
+   Based of the solution for the Transportation problem as described in
+   "Introduction to Mathematical Programming" by F. S. Hillier and 
+   G. J. Lieberman, McGraw-Hill, 1990.
 
-    Copyright (C) 1998 Yossi Rubner
-    Computer Science Department, Stanford University
-    E-Mail: rubner@cs.stanford.edu   URL: http://vision.stanford.edu/~rubner
+   Copyright (C) 1998 Yossi Rubner
+   Computer Science Department, Stanford University
+   E-Mail: rubner@cs.stanford.edu   URL: http://vision.stanford.edu/~rubner
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 */
 
 #ifdef __cplusplus
