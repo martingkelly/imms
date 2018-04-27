40208131130989bf062b27f4aded79f98f8256aa
4SbcAydPtMsqys5m <yyYPbFpymgivhWfE@protonmail.com>
2018-04-27 13:43:43 +0200

    update Audacious plugin source file description

--- clients/audacious/audplugin.cc
+++ clients/audacious/audplugin.cc
@@ -17,7 +17,7 @@
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 */
 /*
- *      The XMMS plugin portion of IMMS
+ *      The Audacious plugin portion of IMMS
  */
 
 #include <string>
